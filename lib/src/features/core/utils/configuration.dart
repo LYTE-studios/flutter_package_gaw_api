fa5a3ebce4bb4a8fa97b9612cbfd6a37dc2a83ba
bteodoru8 <a.teodoru8@gmail.com>
2023-11-07 15:51:29 +0100

    customerCreate call implementation

--- lib/src/features/core/utils/configuration.dart
+++ lib/src/features/core/utils/configuration.dart
@@ -1,9 +1,8 @@
 class Configuration {
   static String clientSecret = '';
 
-  static String apiUrl = 'http://localhost:8000';
-  //static String apiUrl =
-     // 'http://ec2-3-71-206-30.eu-central-1.compute.amazonaws.com';
+  static String apiUrl =
+    'http://ec2-3-71-206-30.eu-central-1.compute.amazonaws.com';
 
   static String? accessToken;
   static String? refreshToken;
